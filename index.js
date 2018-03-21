9e1b9f64af2081a9a903323380fe12593630550b
Tychyna Oleksandr <tychyna.o@gmail.com>
2018-03-21 23:11:06 +0200

    update(*): corrections

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var app = express();
 app.use("/public", express.static("public"));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/views/frontPage.html");
+  res.sendFile(__dirname + "/public/frontPage.html");
 });
 
 app.get('/allCups', function (req, res) {
-  res.sendFile(__dirname + "/views/allCups.html");
+  res.sendFile(__dirname + "/public/allCups.html");
+});
+
+app.get('/singleCup', function (req, res) {
+  res.sendFile(__dirname + "/public/singleCup.html");
+});
+
+app.get('/signUp', function (req, res) {
+  res.sendFile(__dirname + "/public/signUp.html");
 });
 
 app.listen(3000, () => console.log("Server is started"));
